3771e01588b7398f18e829aa971507b5241358e8
lukaa33 <lukabogicevic06@gmail.com>
2023-03-19 21:03:01 +0100

    State and events
    Work with state and events

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,14 +4,22 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+// function clickHandler() {}
+let title = props.title;
+
+   const clickHandler = () => {
+    title = 'Updated!';
+    console.log(title);
+   };
   
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
